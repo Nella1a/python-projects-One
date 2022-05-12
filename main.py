f53f5673d900e81f988faf0c98b19e0e8997085a
Martha <45481873+Nella1a@users.noreply.github.com>
2022-05-12 14:45:12 +0000

    Add three functions

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-print("Hello Word")
+from selenium import webdriver
+import time
+
+
+def open_webdriver():
+  # open Chrome and navigate to url 
+  options = webdriver.ChromeOptions()
+  options.add_argument("disable-infobars")
+  options.add_argument("start-maximized")
+  options.add_argument("disable-dev-shm-usage")
+  options.add_argument("no-sandbox")
+  options.add_experimental_option("excludeSwitches",["enable-automation"])
+  options.add_argument("disable-blink-features=AutomationControlled")
+  driver = webdriver.Chrome(options=options)
+  driver.get("https://titan22.com/")
+  return driver
+
+def get_new_Arrival(driver):
+  section_title = driver.find_element(by="xpath",value="/html/body/main/article/div[2]/section/div/h2")
+  print(section_title.text)
+  new_arrivals = driver.find_element(by="xpath", value="/html/body/main/article/div[2]/section/div/div")
+  print(new_arrivals.text)
+  print(type(new_arrivals.text))
+
+
+def main():
+  driver = open_webdriver()
+  page_title = driver.title
+  print(page_title)
+  get_new_Arrival(driver)
+
+
+main()
