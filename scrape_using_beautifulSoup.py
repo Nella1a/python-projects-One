927ff4f5b76a6df293c55da75eb0bb83fe09aebf
Martha <45481873+Nella1a@users.noreply.github.com>
2022-05-13 08:34:09 +0000

    Add comments

--- scrape_using_beautifulSoup.py
+++ scrape_using_beautifulSoup.py
@@ -3,13 +3,16 @@ from bs4 import BeautifulSoup
 import requests
 
 def get_currency(in_currency, out_currency):
+  # get url with currency request, get response, find currency and print only the integer
   url = f"https://www.x-rates.com/calculator/?from={in_currency}&to={out_currency}&amount=1"
-
   resp = requests.get(url).text
   soup = BeautifulSoup(resp, "html.parser")
-  currency = soup.find("span",class_="ccOutputRslt").get_text()
-  print(currency)
+  rate = soup.find("span",class_="ccOutputRslt").get_text()
+  rate = float(rate.split(" ")[0]) # different option: rate[: -4]
+  return rate
+
+current_rate = get_currency("JPY","USD")
+print(current_rate)
 
 
-
-get_currency("USD","EUR")
+# https://developers.google.com/adsense/management/appendix/currencies.csv
