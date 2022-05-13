b023977566737a77651ab98c6e1a4a425d6b4650
Martha <45481873+Nella1a@users.noreply.github.com>
2022-05-13 08:08:28 +0000

    Add beautifulSoup parser

--- scrape_using_beautifulSoup.py
+++ scrape_using_beautifulSoup.py
@@ -1,8 +1,15 @@
 # scrape realtime currency rates 
 from bs4 import BeautifulSoup
+import requests
 
 def get_currency(in_currency, out_currency):
   url = f"https://www.x-rates.com/calculator/?from={in_currency}&to={out_currency}&amount=1"
-  print(url)
+
+  resp = requests.get(url).text
+  soup = BeautifulSoup(resp, "html.parser")
+  output = soup.select(".ccOutputRslt")
+  print(output)
+
+
 
 get_currency("USD","EUR")
