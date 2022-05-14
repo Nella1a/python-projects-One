d344ebdc828f90528fe520d861d2073dccac9590
Martha <45481873+Nella1a@users.noreply.github.com>
2022-05-14 14:19:50 +0000

    Add comments

--- rest_apis_in_python.py
+++ rest_apis_in_python.py
@@ -6,13 +6,13 @@ my_secret = os.environ['api_key']
 
 
 def get_news(topic, from_date, to_date, language="en", api_key=my_secret):
+  # request news articles, save articles into a list and txt file
   url = f'https://newsapi.org/v2/everything?qInTitle={topic}&from={from_date}&to={to_date}&sortBy=popularity&language={language}&apiKey={api_key}'
   response_data = requests.get(url)
   response = response_data.json()
   articles = response["articles"]
   print(articles)
   news_articles = []
-  
   file = open("articles.txt","w")
   for article in articles:
     news_articles.append(f'\n\ntitle: {article["title"]} \nDescription: {article["description"]}')
@@ -21,11 +21,8 @@ def get_news(topic, from_date, to_date, language="en", api_key=my_secret):
   return news_articles
   
 
-#def check_date_format(from_date,to_date):
- 
-
-
 def main_news():
+# get user news choice 
   topic = input("Choose news topic (space / austria / united states /):  ")
   from_date = input("Choose start date in yyy-mm-dd format: ")
   to_date = input("Choose to date in yyy-mm-dd format: ") 
