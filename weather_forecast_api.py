3a34f94044b0b5096cfb96f22c95455228d68dd5
Martha <45481873+Nella1a@users.noreply.github.com>
2022-05-16 08:16:51 +0000

    Remove unnecessary comments and code

--- weather_forecast_api.py
+++ weather_forecast_api.py
@@ -1,29 +1,27 @@
 # https://openweathermap.org/
 import requests
+import os
 
-api_key='26631f0f41b95fb9f5ac0df9a8f43c92'
+api_key = os.environ["udemy_weatherApp"]
 
-def get_weather(city,lang,api_key=api_key):
-  url=f"http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={api_key}&units=metric&lang={lang}"
+def get_weather(city,lang,metric="metric",api_key=api_key):
+  url=f"http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={api_key}&units={metric}&lang={lang}"
   response_data = requests.get(url)
-  print(response_data)
+  # print(response_data)
   response = response_data.json()
-  print(response)
+  # print(response)
   response_city = response["city"]["name"]
   forecast = response["list"]
-  file = open("weather.txt","a")
-  for weather in forecast:
-    file.write(f'{response_city},{weather["dt_txt"]},{weather["main"]["temp"]},{weather["weather"][0]["description"]}\n\n')
+  with open("weather","a") as file: 
+    for weather in forecast:
+      # extract from response: city, date, temperature, sky condition 
+      file.write(f'{response_city},{weather["dt_txt"]},{weather["main"]["temp"]},{weather["weather"][0]["description"]}\n')
   file.close()
 
 def main_weather():
-  print("hello")
   city = input("city: ")
   lang = input("language (en/de): ")
-  # get_weather(city, lang)
   get_weather(city, lang)
-
-
 
 main_weather()
 
